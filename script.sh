94224507889582ad65bcd1f35cb2f52597565ceb
liuzhen932 🇦🇶 <liuzhen9320@5.1.e.7.0.a.a.e.0.a.2.ip6.arpa>
2025-05-18 00:46:39 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-cache_file=/tmp/dn42_roa_bird2_46.conf
-roa_file="dn42_roa_$(echo "$1" | tr '[:upper:]' '[:lower:]' | tr ':' '-').conf"
+cache_file_v4=/tmp/dn42_roa_bird2_4.conf
+cache_file_v6=/tmp/dn42_roa_bird2_6.conf
+roa_file_v4="dn42_roa_$(echo "$1" | tr '[:upper:]' '[:lower:]' | tr ':' '-')_4.conf"
+roa_file_v6="dn42_roa_$(echo "$1" | tr '[:upper:]' '[:lower:]' | tr ':' '-')_6.conf"
 as_set="$1"
-
-exec &> >(tee $roa_file)
 
 if command -v rg &> /dev/null; then
     search_tool="rg --color=never --no-line-number"
@@ -14,20 +14,36 @@ fi
 # Cache: 1h
 if [[ ! -f "$cache_file" ]] || [[ $(find "$cache_file" -mmin +60 2>/dev/null) ]]; then
     echo "# Updating cache..."
-    curl -q -s -o "$cache_file" https://dn42.burble.com/roa/dn42_roa_bird2_46.conf || {
-        echo "# Error downloading dn42_roa_bird2_46.conf" >&2
+    curl -q -s -o "$cache_file_v4" https://dn42.burble.com/roa/dn42_roa_bird2_4.conf || {
+        echo "# Error downloading dn42_roa_bird2_4.conf" >&2
+        return 1
+    }
+    curl -q -s -o "$cache_file_v6" https://dn42.burble.com/roa/dn42_roa_bird2_6.conf || {
+        echo "# Error downloading dn42_roa_bird2_6.conf" >&2
         return 1
     }
 fi
 
-cat <<EOF
+cat < EOF > $roa_file_v4
 #
-# liuzhen932 DN42 ROA Generator
+# liuzhen932 DN42 ROA Generator - IPv4
+# Last Updated: $(date -u '+%Y-%m-%d %H:%M:%S.%N %z')
+# AS-SET: $as_set
+#
+EOF
+
+cat < EOF > $roa_file_v6
+#
+# liuzhen932 DN42 ROA Generator - IPv6
 # Last Updated: $(date -u '+%Y-%m-%d %H:%M:%S.%N %z')
 # AS-SET: $as_set
 #
 EOF
 
 whois $as_set | awk '/^members:/{match($0,/AS[0-9]+/); print substr($0,RSTART+2,RLENGTH-2)}' | while read -r asn; do
-    $search_tool "$asn" $cache_file
+    $search_tool "$asn" $cache_file_v4 >> $roa_file_v4
+done
+
+whois $as_set | awk '/^members:/{match($0,/AS[0-9]+/); print substr($0,RSTART+2,RLENGTH-2)}' | while read -r asn; do
+    $search_tool "$asn" $cache_file_v6 >> $roa_file_v6
 done
